2c208db836137162e887d769a9268caf83df556e
Guilherme <silvioej@gmail.com>
2019-04-15 11:49:22 -0300

    Deletando full calendar

--- fullcalendar-listar-dados/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$servidor = "localhost";
-	$usuario = "root";
-	$senha = "ifpe";
-	$dbname = "calendario";
-	
-	//Criar a conexao
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
