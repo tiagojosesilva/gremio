3b87a50d57501a9d827c988979b53896e12b0151
hayleylitis <marcelacristinarm96@gmail.com>
2019-04-16 02:18:05 +0000

    calendario

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <!-- Mobile Specific Meta -->
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <!-- Favicon-->
-  <link rel="shortcut icon" href="img/Amor.png" />
+  <link rel="shortcut icon" href="img/logo.jpeg" />
   <!-- Author Meta -->
   <meta name="author" content="CodePixar" />
   <!-- Meta Description -->
