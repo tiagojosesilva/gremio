49dd373bc70f316ab4c17c1b9879b10a890334c7
Guilherme <gui.gcs13@gmail.com>
2019-04-23 12:15:51 -0300

    Alterando calendario

--- pages/novocalendario/calendario.php
+++ pages/novocalendario/calendario.php
@@ -4,44 +4,10 @@ $result_events = "SELECT id, title, color, start, end FROM events";
 $resultado_events = mysqli_query($conn, $result_events);
 ?>
 <!DOCTYPE html>
-<html lang="pt-br" class="no-js">
+<html lang="pt-br">
 	<head>
-	<style type="text/css">
-		#foter{
-			width: 100%	;
-			height: 400px;
-			background-color: black;
-          }
-          #menuu{
-          	text-align: center;
-          	padding: -40px;
-          	
-
-          }
-	</style>
-	<div id="menuu">
-	<?php include '../../components/header-menu.php' ?>
-	</div>
-  <!-- Mobile Specific Meta -->
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <!-- Favicon-->
-  <link rel="shortcut icon" href="../img/amor.png" />
-  <!-- Author Meta -->
-  <meta name="author" content="CodePixar" />
-  <!-- Meta Description -->
-  <meta name="description" content="" />
-  <!-- Meta Keyword -->
-  <meta name="keywords" content="" />
-  <!-- meta character set -->
-  <meta charset="UTF-8" />
-  <!-- Site Title -->
-  <title>Campus do Amor</title>
-
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600|Playfair+Display:700,700i" rel="stylesheet" />
-  <link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
 		<meta charset='utf-8' />
 		<title>Agenda</title>
-		<link rel="stylesheet" href="css/linearicons.css" />
 		<link href='css/bootstrap.min.css' rel='stylesheet'>
 		<link href='css/fullcalendar.min.css' rel='stylesheet' />
 		<link href='css/fullcalendar.print.min.css' rel='stylesheet' media='print' />
@@ -51,14 +17,6 @@ $resultado_events = mysqli_query($conn, $result_events);
 		<script src='js/moment.min.js'></script>
 		<script src='js/fullcalendar.min.js'></script>
 		<script src='locale/pt-br.js'></script>
-		<link rel="stylesheet" href="css/font-awesome.min.css" />
-  <link rel="stylesheet" href="css/magnific-popup.css" />
-  <link rel="stylesheet" href="css/nice-select.css" />
-  <link rel="stylesheet" href="css/owl.carousel.css" />
-  <link rel="stylesheet" href="css/bootstrap.css" />
-  <link rel="stylesheet" href="css/bootstrap-datepicker.css" />
-  <link rel="stylesheet" href="css/themify-icons.css" />
-  <link rel="stylesheet" href="css/main.css" />
 		<script>
 			$(document).ready(function() {
 				$('#calendar').fullCalendar({
@@ -73,7 +31,6 @@ $resultado_events = mysqli_query($conn, $result_events);
 					eventLimit: true, // allow "more" link when too many events
 					eventClick: function(event) {
 						
-						$('#visualizar #id').text(event.id);
 						$('#visualizar #title').text(event.title);
 						$('#visualizar #start').text(event.start.format('DD/MM/YYYY HH:mm:ss'));
 						$('#visualizar #end').text(event.end.format('DD/MM/YYYY HH:mm:ss'));
@@ -100,7 +57,6 @@ $resultado_events = mysqli_query($conn, $result_events);
 		</script>
 	</head>
 	<body>
-	<?php include 'components/header-menu.php' ?>
 
 		<div id='calendar'></div>
 
@@ -113,8 +69,6 @@ $resultado_events = mysqli_query($conn, $result_events);
 					</div>
 					<div class="modal-body">
 						<dl class="dl-horizontal">
-							<dt>ID do Evento</dt>
-							<dd id="id"></dd>
 							<dt>Titulo do Evento</dt>
 							<dd id="title"></dd>
 							<dt>Inicio do Evento</dt>
@@ -125,10 +79,6 @@ $resultado_events = mysqli_query($conn, $result_events);
 					</div>
 				</div>
 			</div>
-		</div>
-		<div id="foter">
-		
-		<?php include '../../components/footer.php' ?>
 		</div>
 	</body>
 </html>
--- pages/novocalendario/events.sql
+++ pages/novocalendario/events.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.6.6deb5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Tempo de geração: 14/11/2017 às 00:12
--- Versão do servidor: 5.7.14
--- Versão do PHP: 7.0.10
+-- Host: localhost:3306
+-- Generation Time: 23-Abr-2019 às 12:14
+-- Versão do servidor: 5.7.24-0ubuntu0.18.04.1
+-- PHP Version: 7.0.33-1+ubuntu18.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `calendario`
+-- Database: `calendario`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `events`
+-- Estrutura da tabela `events`
 --
 
 CREATE TABLE `events` (
@@ -32,35 +32,36 @@ CREATE TABLE `events` (
   `color` varchar(10) DEFAULT NULL,
   `start` datetime DEFAULT NULL,
   `end` datetime DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Fazendo dump de dados para tabela `events`
+-- Extraindo dados da tabela `events`
 --
 
 INSERT INTO `events` (`id`, `title`, `color`, `start`, `end`) VALUES
-(1, 'Reuniao', '#0071c5', '2017-11-23 09:00:00', '2017-11-23 11:00:00'),
-(2, 'Palestra', '#40e0d0', '2017-11-13 14:00:00', '2017-11-13 17:00:00');
+(1, 'Palestra', '#DF7401', '2019-04-24 10:00:00', '2019-04-24 14:00:00'),
+(3, 'Jogos', '#7401DF', '2019-04-29 12:00:00', '2019-04-29 17:00:00'),
+(4, 'Evento', '#DF0101', '2019-04-26 12:00:00', '2019-04-26 13:24:00');
 
 --
--- Índices de tabelas apagadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices de tabela `events`
+-- Indexes for table `events`
 --
 ALTER TABLE `events`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de tabelas apagadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `events`
+-- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
