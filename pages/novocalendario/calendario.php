8b1b03e4b2fd83d67717960b5071df9bdf2c2b3c
Guilherme <gui.gcs13@gmail.com>
2019-05-06 09:58:30 -0300

    Adicionando para o usuario opcao de coress para eventos

--- pages/novocalendario/calendario.php
+++ pages/novocalendario/calendario.php
@@ -41,8 +41,8 @@ $result_events ->execute();
 					selectable: true,
 					selectHelper: true,
 					select: function(start, end){
-						$('#cadastrar #start').val(moment(start)).format(('DD/MM/YYYY HH:mm:ss'));
-						$('#cadastrar #end').val(moment(end)).format(('DD/MM/YYYY HH:mm:ss'));
+						$('#cadastrar #start').val(moment(start).format('DD/MM/YYYY HH:mm:ss'));
+						$('#cadastrar #end').val(moment(end).format('DD/MM/YYYY HH:mm:ss'));
 						$('#cadastrar').modal('show');
 							
 					},
@@ -62,6 +62,41 @@ $result_events ->execute();
 					]
 				});
 			});
+
+			//Máscara para o campo data e hora
+			function DataHora(evento, objeto){
+				var keypress=(window,event)?event.keyCode:evento.which;
+				campo=eval (objeto);
+				if (campo.value== '00/00/0000 00:00:00'){
+					campo.value=""
+				}
+
+				caracteres = '0123456789';
+				separacao1 = '/';
+				separacao2 = ' ';
+				separacao3 = ':';
+				conjunto1 = 2;
+				conjunto2 = 5;
+				conjunto3 = 10;
+				conjunto4 = 13;
+				conjunto5 = 16;
+				if ((caracteres.search(String.fromCharCode (keypress))!=-1) && campo.value.length < (19)){
+					if (campo.value.length == conjunto1 )
+					campo.value = campo.value + separacao1;
+					else if (campo.value.length == conjunto2)
+					campo.value = campo.value + separacao1;
+					else if (campo.value.length == conjunto3)
+					campo.value = campo.value + separacao2;
+					else if (campo.value.length == conjunto4)
+					campo.value = campo.value + separacao3;
+					else if (campo.value.length == conjunto5)
+					campo.value = campo.value + separacao3;
+				}else{
+					event.returnValue = false;
+				}
+			}
+			 
+
 		</script>
 	</head>
 	<body>
@@ -84,6 +119,65 @@ $result_events ->execute();
 							<dt>Fim do Evento</dt>
 							<dd id="end"></dd>
 						</dl>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="modal fade" id="cadastrar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" data-backdrop="static">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title text-center">Cadastrar</h4>
+					</div>
+					<div class="modal-body">
+
+						<form class="form-horizontal">
+  							<div class="form-group">
+   							 <label for="inputEmail3" class="col-sm-2 control-label">Titulo</label>
+   									 <div class="col-sm-10">
+     								 <input type="text" class="form-control" name="titulo" placeholder="Titulo de Evento">
+   									 </div>
+  							</div>
+  							<div class="form-group">
+   							 <label for="inputEmail3" class="col-sm-2 control-label">Cor</label>
+   									 <div class="col-sm-10">
+	   									 <select name="color" class="form-control" id="color">
+													<option value="">Selecione</option>			
+													<option style="color:#FFD700;" value="#FFD700">Amarelo</option>
+													<option style="color:#0071c5;" value="#0071c5">Azul Turquesa</option>
+													<option style="color:#FF4500;" value="#FF4500">Laranja</option>
+													<option style="color:#8B4513;" value="#8B4513">Marrom</option>	
+													<option style="color:#1C1C1C;" value="#1C1C1C">Preto</option>
+													<option style="color:#436EEE;" value="#436EEE">Royal Blue</option>
+													<option style="color:#A020F0;" value="#A020F0">Roxo</option>
+													<option style="color:#40E0D0;" value="#40E0D0">Turquesa</option>										
+													<option style="color:#228B22;" value="#228B22">Verde</option>
+													<option style="color:#8B0000;" value="#8B0000">Vermelho</option>
+											</select>
+   									 </div>
+  							</div>
+  							<div class="form-group">
+   							 <label for="inputEmail3" class="col-sm-2 control-label">Data Inicial</label>
+   									 <div class="col-sm-10">
+     								 <input type="text" class="form-control" name="start" id="start" onkeypress="DataHora(event, this)">
+   									 </div>
+  							</div>
+  							<div class="form-group">
+   							 <label for="inputEmail3" class="col-sm-2 control-label">Data Final</label>
+   									 <div class="col-sm-10">
+     								 <input type="text" class="form-control" name="end" id="end">
+   									 </div>
+  							</div>
+							 
+							  <div class="form-group">
+							    <div class="col-sm-offset-2 col-sm-10">
+							      <button type="submit" class="btn btn-success">Cadastrar</button>
+							    </div>
+							  </div>
+							</form>
+					
 					</div>
 				</div>
 			</div>
