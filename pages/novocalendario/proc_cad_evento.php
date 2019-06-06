1455843a19eac234504b502f3ffebaf58136f612
Guilherme <gui.gcs13@gmail.com>
2019-06-06 12:45:32 -0300

    Recebendo dados do cadastro

--- pages/novocalendario/proc_cad_evento.php
+++ pages/novocalendario/proc_cad_evento.php
@@ -10,44 +10,19 @@ $color = filter_input(INPUT_POST, 'color', FILTER_SANITIZE_STRING);
 $start = filter_input(INPUT_POST, 'start', FILTER_SANITIZE_STRING);
 $end = filter_input(INPUT_POST, 'end', FILTER_SANITIZE_STRING);
 
-//if(!empty($title) && !empty($color) && !empty($start) && !empty($end)){
-		//Converter a data e hora do formato brasileiro para o formato do Banco de Dados
-// 	$data = explode(" ", $start); 
-// 	list($date, $hora) = $data;
-// 	$data_sem_barra = array_reverse(explode("/", $date));
-// 	$data_sem_barra = implode("-", $data_sem_barra);	
-// 	$start_sem_barra =	$data_sem_barra . " " . $hora;
-
-// 	$data = explode(" ", $end);
-// 	list($date, $hora) = $data;
-// 	$data_sem_barra = array_reverse(explode("/", $date));
-// 	$data_sem_barra = implode("-", $data_sem_barra);	
-// 	$end_sem_barra =	$data_sem_barra . " " . $hora;
-
-// 	$result_events = "INSERT INTO events (title, color, start, end) VALUES ('$title', '$color', '$start_sem_barra', '$end_sem_barra')";
-// 	$resultado_events = mysqli_query($conn, $result_events);
-
-// 	//Verificar se salvou no banco de dados através "mysqli_insert_id" o qual verifica se existe o ID do último dado inserido
-
-// 	if(mysqli_insert_id($conn)){
-// 		$_SESSION['msg'] = "<div class='alert alert-success' role='alert'>Evento cadastrado com sucesso <button type='button class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
-// 	header("Location: calendario.php");
-
-// 	} else {
-// 		$_SESSION['msg'] = "<div class='alert alert-danger' role='alert'>Erro ao cadastrar o evento <button type='button class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
-// 	header("Location: calendario.php");
-
-// 	}
-
-// } else {
-// 	$_SESSION['msg'] = "<div class='alert alert-danger' role='alert'>Erro ao cadastrar o evento <button type='button class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
-// 	header("Location: calendario.php");
-// }
-	$a = $conn -> prepare("INSERT INTO events SET title=?, color=?, start=?, end=?");
-	$b = $a -> execute([$title, $color, $start, $end]); 
-	echo 'OK';
+if(!empty($title) && !empty($local) && !empty($color) && !empty($start) && !empty($end)){
+	//Converter a data e hora do formato brasileiro para o formato do Banco de Dados
+	$data = explode(" ", $start);
+	list($date, $hora) = $data;
+	$data_sem_barra = array_reverse(explode("/", $date));
+	$data_sem_barra = implode("-", $data_sem_barra);
+	$start_sem_barra = $data_sem_barra . " " . $hora;
 	
-// }
-// echo 'ERRO';
-
-?>
+	$data = explode(" ", $end);
+	list($date, $hora) = $data;
+	$data_sem_barra = array_reverse(explode("/", $date));
+	$data_sem_barra = implode("-", $data_sem_barra);
+	$end_sem_barra = $data_sem_barra . " " . $hora;
+	
+	$result_events = "INSERT INTO events (title, local, color, start, end) VALUES ('$title', '$local', '$color', '$start_sem_barra', '$end_sem_barra')";
+	$resultado_events = mysqli_query($conn, $result_events);
