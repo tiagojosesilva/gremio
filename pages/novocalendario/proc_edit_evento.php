03bba6788650a751aad870bb6ae17f8a7666d0f2
Guilherme <gui.gcs13@gmail.com>
2019-06-06 12:51:23 -0300

    Restrições de data

--- pages/novocalendario/proc_edit_evento.php
+++ pages/novocalendario/proc_edit_evento.php
@@ -10,3 +10,17 @@ $local = filter_input(INPUT_POST, 'local', FILTER_SANITIZE_STRING);
 $color = filter_input(INPUT_POST, 'color', FILTER_SANITIZE_STRING);
 $start = filter_input(INPUT_POST, 'start', FILTER_SANITIZE_STRING);
 $end = filter_input(INPUT_POST, 'end', FILTER_SANITIZE_STRING);
+
+if(!empty($id) && !empty($title) && !empty($local) && !empty($color) && !empty($start) && !empty($end)){
+	//Converter a data e hora do formato brasileiro para o formato do Banco de Dados
+	$data = explode(" ", $start);
+	list($date, $hora) = $data;
+	$data_sem_barra = array_reverse(explode("/", $date));
+	$data_sem_barra = implode("-", $data_sem_barra);
+	$start_sem_barra = $data_sem_barra . " " . $hora;
+	
+	$data = explode(" ", $end);
+	list($date, $hora) = $data;
+	$data_sem_barra = array_reverse(explode("/", $date));
+	$data_sem_barra = implode("-", $data_sem_barra);
+	$end_sem_barra = $data_sem_barra . " " . $hora;
