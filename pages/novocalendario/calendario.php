eb43b5d87c0117ea3a4dcaf158491bb3e35dd54a
Guilherme <gui.gcs13@gmail.com>
2019-06-06 12:58:58 -0300

    Adicionando campo de local nos inputs

--- pages/novocalendario/calendario.php
+++ pages/novocalendario/calendario.php
@@ -32,28 +32,37 @@ $resultado_events = mysqli_query($conn, $result_events);
 					eventLimit: true, // allow "more" link when too many events
 					eventClick: function(event) {
 						
+						$('#visualizar #id').text(event.id);
+						$('#visualizar #id').val(event.id);
 						$('#visualizar #title').text(event.title);
+						$('#visualizar #title').val(event.title);
+						$('#visualizar #local').text(event.local);
+						$('#visualizar #local').val(event.local);
 						$('#visualizar #start').text(event.start.format('DD/MM/YYYY HH:mm:ss'));
+						$('#visualizar #start').val(event.start.format('DD/MM/YYYY HH:mm:ss'));
 						$('#visualizar #end').text(event.end.format('DD/MM/YYYY HH:mm:ss'));
+						$('#visualizar #end').val(event.end.format('DD/MM/YYYY HH:mm:ss'));
+						$('#visualizar #color').val(event.color);
 						$('#visualizar').modal('show');
 						return false;
 
 					},
+					
 					selectable: true,
 					selectHelper: true,
 					select: function(start, end){
 						$('#cadastrar #start').val(moment(start).format('DD/MM/YYYY HH:mm:ss'));
 						$('#cadastrar #end').val(moment(end).format('DD/MM/YYYY HH:mm:ss'));
-						$('#cadastrar').modal('show');
-							
+						$('#cadastrar').modal('show');						
 					},
-
 					events: [
 						<?php
-							while($row_events = $result_events->fetch()){ ?>
+							while($row_events = mysqli_fetch_array($resultado_events)){
+								?>
 								{
 								id: '<?php echo $row_events['id']; ?>',
 								title: '<?php echo $row_events['title']; ?>',
+								local: '<?php echo $row_events['local']; ?>',
 								start: '<?php echo $row_events['start']; ?>',
 								end: '<?php echo $row_events['end']; ?>',
 								color: '<?php echo $row_events['color']; ?>',
@@ -63,15 +72,15 @@ $resultado_events = mysqli_query($conn, $result_events);
 					]
 				});
 			});
-
-			//Máscara para o campo data e hora
+			
+			//Mascara para o campo data e hora
 			function DataHora(evento, objeto){
 				var keypress=(window.event)?event.keyCode:evento.which;
 				campo = eval (objeto);
 				if (campo.value == '00/00/0000 00:00:00'){
 					campo.value=""
 				}
-
+			 
 				caracteres = '0123456789';
 				separacao1 = '/';
 				separacao2 = ' ';
@@ -96,131 +105,19 @@ $resultado_events = mysqli_query($conn, $result_events);
 					event.returnValue = false;
 				}
 			}
-			 
-
 		</script>
 	</head>
 	<body>
-
 		<div class="container">
 			<div class="page-header">
 				<h1>Agenda</h1>
 			</div>
-
 			<?php
-				if(isset($_SESSION['msg'])){
-					echo $_SESSION['msg'];
-					unset($_SESSION['msg']);
-				}
+			if(isset($_SESSION['msg'])){
+				echo $_SESSION['msg'];
+				unset($_SESSION['msg']);
+			}
 			?>
-
-		<div id='calendar'></div>
+		
+			<div id='calendar'></div>
 		</div>
-
-		<div class="modal fade" id="visualizar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" data-backdrop="static">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title text-center">Dados do Evento</h4>
-					</div>
-					<div class="modal-body">
-						<dl class="dl-horizontal">
-							<dt>Titulo do Evento</dt>
-							<dd id="title"></dd>
-							<dt>Inicio do Evento</dt>
-							<dd id="start"></dd>
-							<dt>Fim do Evento</dt>
-							<dd id="end"></dd>
-						</dl>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="modal fade" id="cadastrar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" data-backdrop="static">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title text-center">Cadastrar Evento</h4>
-					</div>
-					<div class="modal-body">
-
-						<form class="form-horizontal" method="POST" action= "" >
-  							<div class="form-group">
-   							 <label for="inputEmail3" class="col-sm-2 control-label">Titulo</label>
-   									 <div class="col-sm-10">
-     								 <input type="text" class="form-control" name="title" placeholder="Titulo de Evento">
-   									 </div>
-  							</div>
-  							<div class="form-group">
-   							 <label for="inputEmail3" class="col-sm-2 control-label">Cor</label>
-   									 <div class="col-sm-10">
-	   									 <select name="color" class="form-control" id="color">
-													<option value="">Selecione</option>			
-													<option style="color:#FFD700;" value="#FFD700">Amarelo</option>
-													<option style="color:#0071c5;" value="#0071c5">Azul Turquesa</option>
-													<option style="color:#FF4500;" value="#FF4500">Laranja</option>
-													<option style="color:#8B4513;" value="#8B4513">Marrom</option>	
-													<option style="color:#1C1C1C;" value="#1C1C1C">Preto</option>
-													<option style="color:#436EEE;" value="#436EEE">Royal Blue</option>
-													<option style="color:#A020F0;" value="#A020F0">Roxo</option>
-													<option style="color:#40E0D0;" value="#40E0D0">Turquesa</option>										
-													<option style="color:#228B22;" value="#228B22">Verde</option>
-													<option style="color:#8B0000;" value="#8B0000">Vermelho</option>
-											</select>
-   									 </div>
-  							</div>
-  							<div class="form-group">
-   							 <label for="inputEmail3" class="col-sm-2 control-label">Data Inicial</label>
-   									 <div class="col-sm-10">
-     								 <input type="date" class="form-control" name="start" id="start" >
-   									 </div>
-  							</div>
-  							<div class="form-group">
-   							 <label for="inputEmail3" class="col-sm-2 control-label">Data Final</label>
-   									 <div class="col-sm-10">
-     								 <input type="date" class="form-control" name="end" id="end">
-   									 </div>
-  							</div>
-							 
-							  <div class="form-group">
-							    <div class="col-sm-offset-2 col-sm-10">
-							      <button type="submit" class="btn btn-success">Cadastrar</button>
-							    </div>
-							  </div>
-							</form>
-					
-					</div>
-				</div>
-			</div>
-		</div>
-		<script type="text/javascript"> 
-		$(document).ready(function() {
-			
-			$('.btn, btn-success').click(function(event) {
-				event.preventDefault();
-				var title = $('input[name=title]').val();
-				var start = $('input[name=start]').val();
-				var end = $('input[name=end]').val();
-				var color = $('select[name=color]').val();
-				//var dados = $('.form-horizontal').serialize();
-				$.ajax({
-					url: 'proc_cad_evento.php',
-					type: 'POST',
-					data: {title: title, start: start, end: end, color: color},
-					success: function(retorno){
-						if (retorno == 'OK') {
-							//alert('OK');
-							location.reload();
-						}
-					}
-				});
-				
-				
-			});
-		});
-		</script>
-	</body>
-</html>
