60288c8a6d3eb524ee7f38695344e3abcfd303ae
Guilherme <gui.gcs13@gmail.com>
2019-06-06 12:50:17 -0300

    Adicionando arquivo de receber dados para editar o evento

---
+++ pages/novocalendario/proc_edit_evento.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+//Incluir conexao com BD
+include_once("conexao.php");
+
+$id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+$title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING);
+$local = filter_input(INPUT_POST, 'local', FILTER_SANITIZE_STRING);
+$color = filter_input(INPUT_POST, 'color', FILTER_SANITIZE_STRING);
+$start = filter_input(INPUT_POST, 'start', FILTER_SANITIZE_STRING);
+$end = filter_input(INPUT_POST, 'end', FILTER_SANITIZE_STRING);
