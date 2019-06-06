1970a1bf7f278cd1e541c498e0833c85cfb99730
Guilherme <gui.gcs13@gmail.com>
2019-06-06 13:00:46 -0300

    Adicionando Edição de eventos

--- pages/novocalendario/calendario.php
+++ pages/novocalendario/calendario.php
@@ -121,3 +121,161 @@ $resultado_events = mysqli_query($conn, $result_events);
 		
 			<div id='calendar'></div>
 		</div>
+
+		<div class="modal fade" id="visualizar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" data-backdrop="static">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title text-center">Dados do Evento</h4>
+					</div>
+					<div class="modal-body">
+						<div class="visualizar">
+							<dl class="dl-horizontal">
+								<dt>Titulo do Evento</dt>
+								<dd id="title"></dd>
+								<dt>Local do Evento</dt>
+								<dd id="local"></dd>
+								<dt>Inicio do Evento</dt>
+								<dd id="start"></dd>
+								<dt>Fim do Evento</dt>
+								<dd id="end"></dd>
+								<dt>Número do Evento</dt>
+								<dd id="id"></dd>
+							</dl>
+							<button class="btn btn-canc-vis btn-warning">Editar</button>
+						</div>
+						<div class="form" style="display: none">
+							<form class="form-horizontal" method="POST" action="proc_edit_evento.php">
+								<div class="form-group">
+									<label for="inputEmail3" class="col-sm-2 control-label">Titulo</label>
+									<div class="col-sm-10">
+										<input type="text" class="form-control" name="title" id="title" placeholder="Titulo do Evento">
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="inputEmail3" class="col-sm-2 control-label">Local</label>
+									<div class="col-sm-10">
+										<input type="text" class="form-control" name="local" id="local" placeholder="Local do Evento">
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="inputEmail3" class="col-sm-2 control-label">Cor</label>
+									<div class="col-sm-10">
+										<select name="color" class="form-control" id="color">
+											<option value="">Selecione</option>			
+											<option style="color:#FFD700;" value="#FFD700">Amarelo</option>
+											<option style="color:#0071c5;" value="#0071c5">Azul Turquesa</option>
+											<option style="color:#FF4500;" value="#FF4500">Laranja</option>
+											<option style="color:#8B4513;" value="#8B4513">Marrom</option>	
+											<option style="color:#1C1C1C;" value="#1C1C1C">Preto</option>
+											<option style="color:#436EEE;" value="#436EEE">Royal Blue</option>
+											<option style="color:#A020F0;" value="#A020F0">Roxo</option>
+											<option style="color:#40E0D0;" value="#40E0D0">Turquesa</option>										
+											<option style="color:#228B22;" value="#228B22">Verde</option>
+											<option style="color:#8B0000;" value="#8B0000">Vermelho</option>
+										</select>
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="inputEmail3" class="col-sm-2 control-label">Data Inicial</label>
+									<div class="col-sm-10">
+										<input type="text" class="form-control" name="start" id="start" onKeyPress="DataHora(event, this)">
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="inputEmail3" class="col-sm-2 control-label">Data Final</label>
+									<div class="col-sm-10">
+										<input type="text" class="form-control" name="end" id="end" onKeyPress="DataHora(event, this)">
+									</div>
+								</div>
+								<input type="hidden" class="form-control" name="id" id="id">
+								<div class="form-group">
+									<div class="col-sm-offset-2 col-sm-10">
+										<button type="button" class="btn btn-canc-edit btn-primary">Cancelar</button>
+										<button type="submit" class="btn btn-warning">Salvar Alterações</button>
+									</div>
+								</div>
+							</form>
+							
+						
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="modal fade" id="cadastrar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" data-backdrop="static">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title text-center">Cadastrar Evento</h4>
+					</div>
+					<div class="modal-body">
+						<form class="form-horizontal" method="POST" action="proc_cad_evento.php">
+							<div class="form-group">
+								<label for="inputEmail3" class="col-sm-2 control-label">Titulo</label>
+								<div class="col-sm-10">
+									<input type="text" class="form-control" name="title" placeholder="Titulo do Evento">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputEmail3" class="col-sm-2 control-label">Local</label>
+								<div class="col-sm-10">
+									<input type="text" class="form-control" name="local" placeholder="Local do Evento">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputEmail3" class="col-sm-2 control-label">Cor</label>
+								<div class="col-sm-10">
+									<select name="color" class="form-control" id="color">
+										<option value="">Selecione</option>			
+										<option style="color:#FFD700;" value="#FFD700">Amarelo</option>
+										<option style="color:#0071c5;" value="#0071c5">Azul Turquesa</option>
+										<option style="color:#FF4500;" value="#FF4500">Laranja</option>
+										<option style="color:#8B4513;" value="#8B4513">Marrom</option>	
+										<option style="color:#1C1C1C;" value="#1C1C1C">Preto</option>
+										<option style="color:#436EEE;" value="#436EEE">Royal Blue</option>
+										<option style="color:#A020F0;" value="#A020F0">Roxo</option>
+										<option style="color:#40E0D0;" value="#40E0D0">Turquesa</option>										
+										<option style="color:#228B22;" value="#228B22">Verde</option>
+										<option style="color:#8B0000;" value="#8B0000">Vermelho</option>
+									</select>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputEmail3" class="col-sm-2 control-label">Data Inicial</label>
+								<div class="col-sm-10">
+									<input type="text" class="form-control" name="start" id="start" onKeyPress="DataHora(event, this)">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputEmail3" class="col-sm-2 control-label">Data Final</label>
+								<div class="col-sm-10">
+									<input type="text" class="form-control" name="end" id="end" onKeyPress="DataHora(event, this)">
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-sm-offset-2 col-sm-10">
+									<button type="submit" class="btn btn-success">Cadastrar</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<script>
+			$('.btn-canc-vis').on("click", function() {
+				$('.form').slideToggle();
+				$('.visualizar').slideToggle();
+			});
+			$('.btn-canc-edit').on("click", function() {
+				$('.visualizar').slideToggle();
+				$('.form').slideToggle();
+			});
+		</script>
+	</body>
+</html>
